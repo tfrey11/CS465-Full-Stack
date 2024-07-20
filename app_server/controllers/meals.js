1b6ea591fdc331305347191113908a13cb7a4333
Tyler Frey <tylerfrey2011@gmail.com>
2024-07-20 01:29:11 -0500

    Module 3 Completed Baseline

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var mealsData = JSON.parse(fs.readFileSync('./data/meals.json', 'utf-8'));
+
 /* Get Meals Page */
 const meals = (req, res) => {
-    res.render('meals', { title: "Travlr Getaways"});
+    res.render('meals', { title: "Travlr Getaways", mealsData});
 };
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var roomsData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf-8'));
+
 /* Get rooms Page */
 const rooms = (req, res) => {
-    res.render('rooms', { title: "Travlr Getaways"});
+    res.render('rooms', { title: "Travlr Getaways", roomsData});
 };
 
 module.exports = {
