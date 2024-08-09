7ef06713c81f4f0523a0d3d882afbe0f385daab3
Tyler Frey <tylerfrey2011@gmail.com>
2024-08-09 11:21:55 -0500

    Module 6 completed baseline

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// Enable CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 // using specified routers
 app.use('/', indexRouter);
 app.use('/index', indexRouter);
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,14 +1,21 @@
 const express = require('express'); //Express app
-const router = express.Router();
+const router = express.Router(); // Router logic
 
 // This is where we import the controllers we will route
 const tripsController = require('../controllers/trips');
 
 // define route for our trips endpoint
-router.route('/trips').get(tripsController.tripsList); // GET Method routes triplist
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes triplist
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
 
 // GET Method routes tripsFindByCode - requires parameter
+// PUT Method routes tripsUpdateTrip - requires parameter
 
-router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
